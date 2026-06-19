30a035a9932cb3149175102bd196d3916e839dc3
bornface Kangombe <bornface.kangombe@cs.unza.zm>
2026-06-19 13:37:08 +0200

    Update ForemanDashboard.jsx

--- frontend/src/pages/dashboards/ForemanDashboard.jsx
+++ frontend/src/pages/dashboards/ForemanDashboard.jsx
@@ -6,6 +6,7 @@ import {
 import { Link } from 'react-router-dom';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import api from '../../api/axios';
+import DeliveryNote from '../../components/DeliveryNote';
 
 const ForemanDashboard = () => {
   const [loading, setLoading] = useState(true);
@@ -48,6 +49,9 @@ const ForemanDashboard = () => {
           Refresh
         </Button>
       </Box>
+
+      <DeliveryNote />
+
       <Typography variant="subtitle1" gutterBottom>Site Supervision & Workforce</Typography>
 
       {loading ? (
