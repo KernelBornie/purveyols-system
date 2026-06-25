cc48d5bccdaefc3dfe308d39edcfd6dea8b62cff
Your Name <you@example.com>
2026-06-26 00:30:48 +0200

    Set Workbox max file size to 100 MiB

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -36,7 +36,7 @@ export default defineConfig({
         ]
       },
       workbox: {
-        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024, // 👈 4 MiB limit
+        maximumFileSizeToCacheInBytes: 100 * 1024 * 1024, // 100 MiB
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
